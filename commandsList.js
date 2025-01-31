756044713b87415ea6b866a3fdca42a660c570d6
Alex Santos de Souza <alexssmusica@gmail.com>
2025-01-31 15:37:59 -0300

    chore: update script release

---
+++ commandsList.js
@@ -0,0 +1,9 @@
+const commandsList = {
+    release: 'semantic-release --no-ci',
+    electronX64: 'npm run prebuild -- --strip --target 20.0.0 --target 21.0.0 --target 22.0.0 --target 23.0.0 --target 24.0.0 --target 25.0.0 --target 26.0.0 --target 27.0.0 --target 28.0.0 --target 29.0.0 --target 30.0.0 --target 31.0.0 --target 32.0.0 --target 33.0.0 --target 34.0.0 --runtime electron  --arch x64',
+    electronIa32: 'npm run prebuild -- --strip --target 20.0.0 --target 21.0.0 --target 22.0.0 --target 23.0.0 --target 24.0.0 --target 25.0.0 --target 26.0.0 --target 27.0.0 --target 28.0.0 --target 29.0.0 --target 30.0.0 --target 31.0.0 --target 32.0.0 --target 33.0.0 --target 34.0.0 --runtime electron  --arch ia32',
+    nodeIa32: 'npm run prebuild -- --strip --target 18.20.6 --target 19.0.0 --target 20.0.0 --target 21.0.0 --target 22.0.0 --arch ia32',
+    nodeX64: 'npm run prebuild -- --strip --target 18.20.6 --target 19.0.0 --target 20.0.0 --target 21.0.0 --target 22.0.0 --arch x64',
+};
+
+module.exports = { commandsList };
--- release.js
+++ release.js
@@ -1,18 +1,26 @@
 const { execSync } = require('child_process');
-const dotEnv = require('dotenv')
+const dotEnv = require('dotenv');
+const { commandsList } = require('./commandsList');
+
 dotEnv.config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
-
 
 try {
     if (!process.env.GH_TOKEN) {
         throw new Error('GH_TOKEN não encontrado no arquivo .env');
     }
     console.log('Iniciando processo de release...');
-    execSync(`cross-env GH_TOKEN=${process.env.GH_TOKEN} semantic-release --no-ci`, {
-        stdio: 'inherit'
-    });
+    const commands = Object.values(commandsList);
+    for (const command of commands) {
+        execSync(command, {
+            stdio: 'inherit',
+            env: {
+                ...process.env,
+                GH_TOKEN: process.env.GH_TOKEN
+            }
+        });
+    }
     console.log('Release concluído com sucesso!');
 } catch (error) {
     console.error('Erro durante o processo de release:', error.message);
